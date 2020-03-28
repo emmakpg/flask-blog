75242925c3ab20aae38256133a651e61c17f3ead
Emmanuel Kpogo <emmkpg@gmail.com>
2020-03-28 00:25:18 +0000

    adding flask blog

---
+++ flaskblog/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '95bdc18f9dca3481a206ee2ff17e5c73'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  #loc of db 
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)   #instance of object for pw encryption
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
+
+
